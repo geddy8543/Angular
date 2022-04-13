dcdb6ac320031a02324a4057eed02cebeee2cbec
Gail Eddy <gail.eddy@Gails-MacBook-Pro.local>
2022-04-13 11:14:13 -0500

    removes listFilter from component export class and replaces with a getter and setter method for private backing variable

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,7 +11,15 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    
+    private _listFilter: string = '';
+    get listFilter():string {
+      return this._listFilter;
+    }
+    set listFilter(value: string) {
+      this._listFilter;
+    }
+
     products: IProduct[] = [
     {
         "productId": 1,
