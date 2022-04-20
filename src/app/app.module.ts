c7f401055119d3442a6c60d8e365a51c4baaa706
Gail Eddy <gail.eddy@kin.com>
2022-04-20 14:00:21 -0500

    cleans up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
 import { ProductModule } from './products/product.module';
@@ -18,7 +16,6 @@ import { ProductModule } from './products/product.module';
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-    
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
